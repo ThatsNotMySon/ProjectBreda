835d535ecf5d0f7a0f498b51ae9b5296bb352ca2
Jasper <j.bonefaas@student.avans.nl>
2019-12-09 12:24:06 +0100

    Added Toolbar to MainActivity

--- app/src/main/java/com/example/myapplication/View/MainActivity.java
+++ app/src/main/java/com/example/myapplication/View/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapplication.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.example.myapplication.R;
 
@@ -12,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar, menu);
+        return true;
     }
 }
