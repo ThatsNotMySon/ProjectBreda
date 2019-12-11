a8da21ce0d823137e7081f978471ccb53ffef407
Joëlle <joelle.a.m.devries@gmail.com>
2019-12-11 22:07:15 +0100

    database finish

--- app/src/main/java/com/example/myapplication/Model/Datamanagement/DatabaseManager.java
+++ app/src/main/java/com/example/myapplication/Model/Datamanagement/DatabaseManager.java
@@ -13,11 +13,11 @@ public class DatabaseManager extends SQLiteOpenHelper {
 
     private static final String TAG = "DatabaseHelper";
 
-    private static final String TABLE_NAME = "color_table";
+    private static final String TABLE_NAME = "waypoint_tabel";
     private static final String COL1 = "ID";
-    private static final String COL2 = "HUE";
-    private static final String COL3 = "SAT";
-    private static final String COL4 = "BRI";
+    private static final String COL2 = "x";
+    private static final String COL3 = "y";
+    private static final String COL4 = "info";
     private static final String[] COLUMS = {COL1, COL2, COL3, COL4};
 
     public DatabaseManager(Context context) {
@@ -26,8 +26,8 @@ public class DatabaseManager extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +"HUE FLOAT, "
-                + "SAT FLOAT, " + "BRI FLOAT )";
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +"x FLOAT, "
+                + "y FLOAT, " + "info STRING )";
         sqLiteDatabase.execSQL(createTable);
     }
 
@@ -37,13 +37,74 @@ public class DatabaseManager extends SQLiteOpenHelper {
         onCreate(sqLiteDatabase);
     }
 
-    public void deleteOne(float hue, float bri, float sat, int id){
+    public void deleteOne(float x, float y, String info, int id){
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         sqLiteDatabase.delete(TABLE_NAME, "ID = ?", new String[] {id +""  });
         sqLiteDatabase.close();
 
     }
 
+    public List<Database> allWaypoints() {
 
-    
+        List<Database> databases = new LinkedList<Database>();
+        String query = "SELECT  * FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        Database database = null;
+
+        if (cursor.moveToFirst()) {
+            do {
+                database = new Database();
+                database.setId(Integer.parseInt(cursor.getString(0)));
+                database.setX(cursor.getFloat(1));
+                database.setY(cursor.getFloat(2));
+                database.setInfo(cursor.getString(3));
+                databases.add(database);
+            } while (cursor.moveToNext());
+        }
+
+        return databases;
+    }
+
+    public Database database(int id){
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        Cursor cursor = sqLiteDatabase.query(TABLE_NAME,
+                COLUMS,
+                " id = ?",
+                new String[]{ String.valueOf(id)},
+                null,
+                null,
+                null,
+                null);
+
+        if(cursor != null){
+            cursor.moveToFirst();
+        }
+
+        Database database = new Database();
+        database.setId(Integer.parseInt(cursor.getString(0)));
+        database.setX(cursor.getFloat(1));
+        database.setY(cursor.getFloat(2));
+        database.setInfo(cursor.getString(3));
+
+        return database;
+    }
+
+
+    public boolean addData(Database database){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2, database.getX());
+        contentValues.put(COL3, database.getY());
+        contentValues.put(COL4, database.getInfo());
+
+        long result = sqLiteDatabase.insert(TABLE_NAME, null, contentValues);
+
+        if(result == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
 }
