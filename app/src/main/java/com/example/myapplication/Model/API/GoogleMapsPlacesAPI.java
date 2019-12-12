26332578330f2d9269500da0582fff36594cd220
Tom <tomkaas74@gmail.com>
2019-12-12 11:50:29 +0100

    Changed API

--- app/src/main/java/com/example/myapplication/Model/API/GoogleMapsPlacesAPI.java
+++ app/src/main/java/com/example/myapplication/Model/API/GoogleMapsPlacesAPI.java
@@ -1,4 +1,10 @@
 package com.example.myapplication.Model.API;
 
+import android.content.Context;
+
 public class GoogleMapsPlacesAPI extends API {
+
+    public GoogleMapsPlacesAPI(Context context) {
+        super(context);
+    }
 }
