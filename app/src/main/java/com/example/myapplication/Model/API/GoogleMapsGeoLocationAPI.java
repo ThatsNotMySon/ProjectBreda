54a6c99b0ce83c065d7a5223f6e327c1d18a3137
Arno Nagtzaam <arno32@live.nl>
2019-12-12 11:59:53 +0100

    Fixed: Added quick fix for default API constructors

--- app/src/main/java/com/example/myapplication/Model/API/GoogleMapsGeoLocationAPI.java
+++ app/src/main/java/com/example/myapplication/Model/API/GoogleMapsGeoLocationAPI.java
@@ -1,4 +1,9 @@
 package com.example.myapplication.Model.API;
 
+import android.content.Context;
+
 public class GoogleMapsGeoLocationAPI extends API {
+    public GoogleMapsGeoLocationAPI(Context context) {
+        super(context);
+    }
 }
--- app/src/main/java/com/example/myapplication/Model/API/GoogleMapsPlacesAPI.java
+++ app/src/main/java/com/example/myapplication/Model/API/GoogleMapsPlacesAPI.java
@@ -1,4 +1,9 @@
 package com.example.myapplication.Model.API;
 
+import android.content.Context;
+
 public class GoogleMapsPlacesAPI extends API {
+    public GoogleMapsPlacesAPI(Context context) {
+        super(context);
+    }
 }
