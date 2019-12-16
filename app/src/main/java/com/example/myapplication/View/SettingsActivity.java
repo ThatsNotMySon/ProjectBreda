e4983dc8272fbcb08eb5936614fd792021e6c473
Jasper <j.bonefaas@student.avans.nl>
2019-12-16 11:28:26 +0100

    Added language selection in SettingsActivity

--- app/src/main/java/com/example/myapplication/View/SettingsActivity.java
+++ app/src/main/java/com/example/myapplication/View/SettingsActivity.java
@@ -5,23 +5,71 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
 
+import java.util.HashMap;
+import java.util.Locale;
+
 public class SettingsActivity extends AppCompatActivity {
 
     private static String TAG = "SettingsActivity";
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        Log.i(TAG, "OnCreate");
+        this.spinner = findViewById(R.id.languageSpinner);
+
+        final String[] languageCodes = new String[]{
+                "en", "nl"
+        };
+
+        final String[] arraySpinner = new String[] {
+                getResources().getString(R.string.english), getResources().getString(R.string.dutch)
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.spinner.setAdapter(adapter);
+
+        this.spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                setAppLocale(languageCodes[i]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    private void setAppLocale(String localeCode){
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN){
+            conf.setLocale(new Locale(localeCode.toLowerCase()));
+        } else {
+            conf.locale = new Locale(localeCode.toLowerCase());
+        }
+        res.updateConfiguration(conf, dm);
     }
 }
